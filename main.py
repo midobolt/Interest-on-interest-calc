a12c341a3f437f0c9dabda9e84e3fbe058eed2e5
Mido <34779315+midobolt@users.noreply.github.com>
2023-12-02 17:37:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ principle = 0
 rate = 0
 time = 0
 
-while principle <= 0:
+while True:
     principle = float(input("enter the principle amount:"))
     if principle < 0:
           print("principle can't be less than zero")
     else:
           break
 
-while rate <= 0:
-    rate = float(input("enter the interest rate:"))
+while True:
+    rate = float(input("enter the Tnterest rate:"))
     if rate < 0:
           print("Interest rate can't be less than zero")
     else:
           break
           
-while time <= 0:
+while True:
     time = float(input("enter the time in years:"))
     if time < 0:
           print("Time can't be less than zero")
